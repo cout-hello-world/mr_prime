f45fae4611760126f2e8126639e3b426f5d449e8
Henry Elliott <henrylovesscience@gmail.com>
2018-05-04 08:57:23 -0500

    Have test replicate a UTM primes page
    Also, this commit adds a .gitignore and a public domain notice in the
    README.md file

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 This code uses the Miller-Rabin primality test to determine if 64-bit integers
 are prime.
+
+This implementation is placed in the public domain by its author, Henry Elliott
--- test.c
+++ test.c
@@ -1,12 +1,25 @@
+// Generate part of table at
+// https://primes.utm.edu/lists/2small/0bit.html
 #include <stdio.h>
 #include <stdint.h>
 #include "mr_prime.h"
 
 int main(void)
 {
-	for (uint64_t i = 1; i != 100000; ++i) {
-		if (mr_prime(0 - i)) {
-			printf("%lu\n", i);
+	for (int n = 8; n != 65; ++n) {
+		printf("%d: ", n);
+		int count = 0;
+		unsigned k = 1;
+		while (count != 10) {
+			if (mr_prime((1ULL << n) - k)) {
+				if (count != 9) {
+					printf("%u, ", k);
+				} else {
+					printf("%u\n", k);
+				}
+				++count;
+			}
+			++k;
 		}
 	}
 }
